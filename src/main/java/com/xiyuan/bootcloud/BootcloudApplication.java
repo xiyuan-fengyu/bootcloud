713f79796f05c8a43265867dc5b5f450680f9ed1
xiyuan_fengyu <xiyuan_fengyu@163.com>
2018-06-15 18:37:58 +0800

    initial

--- src/main/java/com/xiyuan/bootcloud/BootcloudApplication.java
+++ src/main/java/com/xiyuan/bootcloud/BootcloudApplication.java
@@ -3,10 +3,8 @@ package com.xiyuan.bootcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 @MapperScan("com.xiyuan.*")
 public class BootcloudApplication {
 
--- src/main/java/com/xiyuan/bootcloud/CloudConfigServerApplication.java
+++ src/main/java/com/xiyuan/bootcloud/CloudConfigServerApplication.java
@@ -1,17 +1,17 @@
-package com.xiyuan.bootcloud;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.config.server.EnableConfigServer;
-
-@SpringBootApplication
-@ConfigurationProperties("cloudConfigServer.properties")
-@EnableConfigServer
-public class CloudConfigServerApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(CloudConfigServerApplication.class, args);
-    }
-
-}
+//package com.xiyuan.bootcloud;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.cloud.config.server.EnableConfigServer;
+//
+//@SpringBootApplication
+//@ConfigurationProperties("cloudConfigServer.properties")
+//@EnableConfigServer
+//public class CloudConfigServerApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(CloudConfigServerApplication.class, args);
+//    }
+//
+//}
